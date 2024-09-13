1a965bfacfe51a141b2f84d0a27dd759af582c5e
Sharmenserem <shammelody43@gmail.com>
2024-09-13 19:14:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
-#!/usr/bin/bash
-
-apt-get update
-apt-get upgrade
-apt-get install nodejs
-apt-get install libwebp
-apt-get install ffmpeg
-apt-get install wget
-apt-get install tesseract
-wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
-npm install --dev
-
-echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
-echo "Updates : fix Bugs"
+termux-setup-storage
+apt update
+apt upgrade
+pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pkg install bash
+pkg install libwebp
+pkg install git -y
+pkg install nodejs -y 
+pkg install ffmpeg -y 
+pkg install wget
+pkg install imagemagick -y
+git clone https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true
+cd Phantom-bug-bot
+yarn install 
+npm i
+npm start
